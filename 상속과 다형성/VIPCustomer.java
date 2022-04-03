2ccdc6086783be9b3d4badcad58d2fdc74dc5f1c
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-04-03 20:29:55 +0900

    Create VIPCustomer.java

---
+++ 상속과 다형성/VIPCustomer.java
@@ -0,0 +1,29 @@
+package reference;
+
+public class VIPCustomer extends Customer {
+    private int agendID;
+    double saleRatio;
+
+    public VIPCustomer(int customerID, String customerName, int agentID) {
+        super(customerID, customerName);
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+        this.agendID = agentID;
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * saleRatio);
+    }
+
+    @Override
+    public String showCustomerInfo() {
+        return super.showCustomerInfo() + "담당 상담원 번호는 " + agendID + "입니다";
+    }
+
+    public int getAgentID() {
+        return agendID;
+    }
+}
